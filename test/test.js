57d05ec1a18ad9abe27824127edf9dd4675f631a
Emily Kapor <emily@emilyaviva.com>
2015-07-27 15:23:22 -0700

    wrote more boilerplate

--- test/test.js
+++ test/test.js
@@ -7,9 +7,14 @@ var chaiHttp = require('chai-http');
 var expect = chai.expect;
 
 process.env.MONGO_SAFETYAPP_URI = 'mongodb://localhost/mongo_safetyapp_test';
-process.env.PORT = 3003;
+process.env.PORT = process.env.PORT_TEST || 3003;
 
 chai.use(chaiHttp);
+
+describe(server.js, function() {
+  this.timeout(5000);
+
+  // tests go here
 
   after(function(done) {
     mongoose.connection.db.dropDatabase(function() { done(); });

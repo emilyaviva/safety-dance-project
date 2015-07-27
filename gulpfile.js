c4611d8556aa23523d92e87310d1a100b7c84bc5
Peggy Wiltz <hengist2001@hotmail.com>
2015-07-27 10:25:16 -0700

    added build tasks to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -22,4 +22,31 @@ gulp.task('lint', function() {
 
 gulp.task('watch', function() {
   gulp.watch(['!./gulpfile.js', '**/*.js', '!node_modules/**', './package.json'], ['lint']);
+  gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('webpackdev', function() {
+  return gulp.src('./app/js/**/*.js')
+    .pipe(webpack({
+      output: {
+        filename: './js/bundle.js'
+      }
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('copy', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+
+  return gulp.src('./app/**/*.html')
+  .pipe(gulp.dest('./public/'))
+  .pipe(minifyHTML(opts))
+  .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('build', ['copy', 'webpackdev', 'sass']);
+gulp.task('default', ['sass:watch']);

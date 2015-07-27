d42dee6f3cd8b996c749808fa22dc3926b8a5746
Emily Kapor <emily@emilyaviva.com>
2015-07-27 11:17:55 -0700

    Modified license and package.json

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
-var mongoURI = process.env.MONGO_FILESAPP_URI || 'mongodb://localhost/safetydance';
-var mongoURI = 'mongodb://localhost/safetydance';
+var mongoURI = process.env.MONGO_SAFETYAPP_URI || 'mongodb://localhost/safetydance';
 
 var userRoutes = express.Router();
 var fileRoutes = express.Router();
 require('./routes/user-routes')(userRoutes);
-app.use('/', userRoutes);
+app.use('/api', userRoutes);
 
 mongoose.connect(mongoURI, function(err) {
   if (err) console.log('error: ' + err);

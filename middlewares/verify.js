4710e54594e6c61dbf045cacc9c5e1af4932b7df
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2015-07-29 18:30:45 -0700

    fixed block

--- middlewares/verify.js
+++ middlewares/verify.js
@@ -6,11 +6,13 @@ module.exports = function(req, res, next) {
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
   if (token) {
     jwt.verify(token, process.env.secret, function(err, decoded) {
-      if (err) res.json({success: false, msg: 'Failed to authenticate token.'});
-      else {
+      if (err) {
+        res.json({success: false, msg: 'Failed to authenticate token.'});
+      } else {
         req.decoded = decoded;
         next();
       }
     });
+  }
   else res.status(403).send({success: false, msg: 'No token provided.'});
 };

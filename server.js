2e322a931758141e80f23876c42b7fafd389ad1d
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2015-07-30 15:26:33 -0700

    reverted

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 var mongoURI = process.env.MONGO_SAFETYAPP_URI || 'mongodb://localhost/safetydance';
 
